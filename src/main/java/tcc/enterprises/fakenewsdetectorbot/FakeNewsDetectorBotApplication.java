64af2242e46275d49681e7fff501f65871ac2a2f
Davi_Camara <davicamara@unimedceara.com.br>
2023-08-27 22:18:18 -0300

    first commit

---
+++ src/main/java/tcc/enterprises/fakenewsdetectorbot/FakeNewsDetectorBotApplication.java
@@ -0,0 +1,13 @@
+package tcc.enterprises.fakenewsdetectorbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeNewsDetectorBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeNewsDetectorBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tcc/enterprises/fakenewsdetectorbot/FakeNewsDetectorBotApplicationTests.java
@@ -0,0 +1,13 @@
+package tcc.enterprises.fakenewsdetectorbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeNewsDetectorBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
