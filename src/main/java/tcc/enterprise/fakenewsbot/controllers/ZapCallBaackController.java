b780f8a973ceae254b2a7caf99b6da012baa65d6
Davi_Camara <davicamara@unimedceara.com.br>
2023-11-20 20:32:12 -0300

    adicao de endpoint de teste de envio de media para a rede neural e finalizacao de implementacao de envio de media para a rede neural

--- src/main/java/tcc/enterprise/fakenewsbot/controllers/ZapCallBaackController.java
+++ src/main/java/tcc/enterprise/fakenewsbot/controllers/ZapCallBaackController.java
@@ -4,10 +4,16 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import tcc.enterprise.fakenewsbot.Model.MessageCallBack;
 import tcc.enterprise.fakenewsbot.services.ZapCallBaackService;
 
+import java.io.*;
 import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.logging.Logger;
 
 @RestController
@@ -23,21 +29,21 @@ public class ZapCallBaackController {
         logger.info("hello-world");
         return ResponseEntity.ok("hello-world");
     }
+
     @PostMapping("/webhook")
-    public ResponseEntity<String> read(@RequestBody String inputJson) throws JsonProcessingException, URISyntaxException {
+    public ResponseEntity<String> read(@RequestBody String inputJson) throws IOException, URISyntaxException {
 
         MessageCallBack retornoCallback = zapCallBaackService.parseJson(inputJson);
         String retornoHandler = zapCallBaackService.callBackHandler(retornoCallback);
 
         //logger.info(zapCallBaackService.find());
         logger.info("[JSON CALLBACK]--CallBack: " + retornoCallback.toString());
-        logger.info("[JSON retorno HANDLER]--HANDLER:" +retornoHandler);
+        logger.info("[JSON retorno HANDLER]--HANDLER:" + retornoHandler);
         return ResponseEntity.ok("ok");
     }
 
     @Autowired
     private ZapCallBaackService zapCallBaackService;
-
 
     @GetMapping("/webhook")
     public ResponseEntity<Integer> webhookAuthentication(@RequestParam(name = "hub.mode") String hubMode,
@@ -54,12 +60,22 @@ public class ZapCallBaackController {
         }
     }
 
-    @GetMapping("/rede-neural-retorno")
-    public ResponseEntity<String> retornoRedeNeural(@RequestParam(name = "percentual") String percentual,
-                                                    @RequestParam(name = "phoneNumber") String phoneNumberReciever)
-            throws URISyntaxException {
-        //zapCallBaackService.getWhatsAppMediaUrl()
-        zapCallBaackService.sendWhatsappMessage(phoneNumberReciever, percentual);
+
+    @PostMapping("/teste-rede-neural")
+    public ResponseEntity<String> read(@RequestParam("audio") MultipartFile media) throws IOException {
+        media.getName();
+        byte[] strToBytes = media.getBytes();
+
+        zapCallBaackService.sendMediaToRedeNeural(strToBytes);
+
         return ResponseEntity.ok("ok");
     }
+//    @GetMapping("/rede-neural-retorno")
+//    public ResponseEntity<String> retornoRedeNeural(@RequestParam(name = "percentual") String percentual,
+//                                                    @RequestParam(name = "phoneNumber") String phoneNumberReciever)
+//            throws URISyntaxException {
+//        //zapCallBaackService.getWhatsAppMediaUrl()
+//        zapCallBaackService.sendWhatsappMessage(phoneNumberReciever, percentual);
+//        return ResponseEntity.ok("ok");
+//    }
 }
--- src/main/java/tcc/enterprise/fakenewsbot/services/ZapCallBaackService.java
+++ src/main/java/tcc/enterprise/fakenewsbot/services/ZapCallBaackService.java
@@ -3,22 +3,24 @@ package tcc.enterprise.fakenewsbot.services;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 import tcc.enterprise.fakenewsbot.Model.Medias.MediaUrl;
 import tcc.enterprise.fakenewsbot.Model.Message;
 import tcc.enterprise.fakenewsbot.Model.MessageCallBack;
 import tcc.enterprise.fakenewsbot.Model.Messages.MessageAvulsa;
 import tcc.enterprise.fakenewsbot.Model.Messages.Text;
-import tcc.enterprise.fakenewsbot.controllers.ZapCallBaackController;
 import tcc.enterprise.fakenewsbot.util.enums.MessageTypes;
 
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.*;
 import java.util.logging.Logger;
 
 @Service
@@ -48,12 +50,14 @@ public class ZapCallBaackService {
         return callBackJson;
     }
 
-    public String callBackHandler(MessageCallBack messageCallBack) throws URISyntaxException {
+    public String callBackHandler(MessageCallBack messageCallBack) throws URISyntaxException, IOException {
         Message message = messageCallBack.getEntry().get(0).getChanges().get(0).getValue().getMessages().get(0);
 
         if (message.getType() != MessageTypes.TEXT.getDescription()) {
             MediaUrl mediaUrl = getWhatsAppMediaUrl(message.getAudio().getId());
             byte[] media = downloadWhatsAppMedia(mediaUrl);
+            logger.info("mediaaaaaaaaaa" + media);
+
             sendMediaToRedeNeural(media);
 
             //TODO REMOVER QUANDO FOR IMPLEMENTANDO REDE NEURAL
@@ -96,8 +100,59 @@ public class ZapCallBaackService {
 
         return result.getBody();
     }
-    private void sendMediaToRedeNeural(byte[] media) {
-        //TODO: REST CALL TO REDE NEURAL.
+
+    public String sendMediaToRedeNeural(byte[] media) throws IOException {
+
+        byte[] fileBytes = media;
+        String uploadUrl = "http://67.205.179.72:5000/";
+
+        // Create connection
+        URL url = new URL(uploadUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        // Set connection properties
+        connection.setDoOutput(true);
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=boundary");
+
+        // Get the output stream of the connection
+        OutputStream outputStream = connection.getOutputStream();
+
+        writeFormField(outputStream, "audio", "uploaded_audio.ogg", fileBytes);
+
+        // Write the byte array to the output stream
+        outputStream.write(fileBytes);
+
+        // Close the output stream
+        outputStream.close();
+
+        // Get the response code
+        int responseCode = connection.getResponseCode();
+        connection.getContent();
+        System.out.println("Response Code: " + responseCode);
+
+        // Handle the response as needed
+
+        // Close the connection
+        connection.disconnect();
+        return "ok";
+    }
+
+    private static void writeFormField(OutputStream outputStream, String fieldName, String fileName, byte[] fileBytes) throws IOException {
+        String boundary = "boundary";
+        String delimiter = "--" + boundary + "\r\n";
+        String endDelimiter = "\r\n--" + boundary + "--\r\n";
+
+        // Write the form field header
+        outputStream.write(delimiter.getBytes());
+        outputStream.write(("Content-Disposition: form-data; name=\"" + fieldName + "\"; filename=\"" + fileName + "\"\r\n").getBytes());
+        outputStream.write("Content-Type: application/octet-stream\r\n\r\n".getBytes());
+
+        // Write the file content
+        outputStream.write(fileBytes);
+
+        // Write the end delimiter
+        outputStream.write(endDelimiter.getBytes());
     }
 
     //TODO: MODULARIZAR O PARAMETRO DE TEXTO, AVERIGAR SE O NUMERO de telefone DEVERIA VIR DA REDE NEURAL
